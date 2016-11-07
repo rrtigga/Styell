77a7f36b8df6b122c7741a50c61d1b7f136c75ce
rrtigga <rrtigga@ucdavis.edu>
2016-11-06 23:22:14 -0800

    made basic hero page with background cover

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/feed', function(req, res, next) {
+  res.render('feed', { title: 'Express' });
+});
+
 
 module.exports = router;
