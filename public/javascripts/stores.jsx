72f942e50792487bd66f9bfad318cd807a792d94
rrtigga <rrtigga@ucdavis.edu>
2016-12-28 13:17:57 -0800

    clearing the repo fixed

---
+++ public/javascripts/stores.jsx
@@ -0,0 +1,105 @@
+var RepeatModule = React.createClass({
+   getInitialState: function() {
+      return { items: this.props.items || [] }
+   },
+   componentWillMount: function() {
+     console.log("componentWillMount()")
+     this.setState({ items : data })
+     console.log(data,"data is here");
+   },
+   handleClick: function (e, item) {
+   	this.setState({active: true});
+   },
+   render: function() {
+      var listItems = this.state.items.map(function(item) {
+         return (
+            <ListItem item={item}/>
+         );
+      });
+      return (
+         <div className="flex-container">
+             {listItems}
+         </div>
+      );
+   }
+});
+/* make the items stateless */
+var ListItem = function(props) {
+	var backgroundImage = {
+	  backgroundImage: 'url(' + props.item.pictureURL + ')'
+	};
+     return (
+         <div className="block-grid-item">
+		  <a className="display-block card text-gray-darker">
+		    <div style={backgroundImage} className="card-img-wrap m-xs-0 placeholder placeholder-landscape _this.state.activeKey === i ? 'active' : '' ">
+		    </div>
+		    <div className="card-meta">
+		      <div className="vesta-hp-gifts-card-title text-gray-darker">{props.item.storeName}</div>
+		    </div>
+		  </a>
+		</div>
+     );
+}
+var data = [
+	{	
+		"storeName": "Adidas",
+		"pictureURL": "../images/stores/adidas.jpg",
+		"storeURL": "http://www.rohittigga.com/img/realfriends.jpg"
+	},
+	{	
+		"storeName": "American Eagle",
+		"pictureURL": "../images/stores/americaneagle.jpg",
+		"storeURL": "http://www.rohittigga.com/img/realfriends.jpg"
+	},
+	{	
+		"storeName": "ASOS",
+		"pictureURL": "../images/stores/asos.jpg",
+		"storeURL": "http://www.rohittigga.com/img/realfriends.jpg"
+	},
+	{	
+		"storeName": "H&M",
+		"pictureURL": "../images/stores/h&m.jpeg",
+		"storeURL": "http://www.rohittigga.com/img/realfriends.jpg"
+	},
+	{	
+		"storeName": "Nike",
+		"pictureURL": "../images/stores/nike.jpg",
+		"storeURL": "http://www.rohittigga.com/img/realfriends.jpg"
+	},
+	{	
+		"storeName": "Northface",
+		"pictureURL": "../images/stores/northface.jpeg",
+		"storeURL": "http://www.rohittigga.com/img/realfriends.jpg"
+	},
+	{	
+		"storeName": "Obey",
+		"pictureURL": "../images/stores/obey.jpg",
+		"storeURL": "http://www.rohittigga.com/img/realfriends.jpg"
+	},
+	{	
+		"storeName": "Pac Sun",
+		"pictureURL": "../images/stores/pacsun.jpg",
+		"storeURL": "http://www.rohittigga.com/img/realfriends.jpg"
+	},
+	{	
+		"storeName": "Stussy",
+		"pictureURL": "../images/stores/stussy.jpeg",
+		"storeURL": "http://www.rohittigga.com/img/realfriends.jpg"
+	},
+	{	
+		"storeName": "Super Dry",
+		"pictureURL": "../images/stores/superdry.gif",
+		"storeURL": "http://www.rohittigga.com/img/realfriends.jpg"
+	},
+	{	
+		"storeName": "Supreme",
+		"pictureURL": "../images/stores/supreme.jpg",
+		"storeURL": "http://www.rohittigga.com/img/realfriends.jpg"
+	},
+	{	
+		"storeName": "Uniqlo",
+		"pictureURL": "../images/stores/uniqlo.jpg",
+		"storeURL": "http://www.rohittigga.com/img/realfriends.jpg"
+	}
+];
+ReactDOM.render(<RepeatModule items={data} />, document.getElementById('stores-content'));
